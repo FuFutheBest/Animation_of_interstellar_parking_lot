2332363e9332b6073cd03188bfd10580883bd642
fufu <2741835851@sjtu.edu.cn>
2025-01-09 23:01:16 +0800

    new setup

--- src/figure.cpp
+++ src/figure.cpp
@@ -1,76 +1,80 @@
 #include "figure.h"
 Vec::Vec(float _x, float _y) {
-	this->x = _x;
-	this->y = _y;
+  this->x = _x;
+  this->y = _y;
 }
 
 float Vec::getX() { return this->x; }
 float Vec::getY() { return this->y; }
 
-Vec Vec::operator+(const Vec& other) { return Vec(this->x + other.x, this->y + other.y); }
-Vec Vec::operator-(const Vec& other) { return Vec(this->x - other.x, this->y - other.y); }
+Vec Vec::operator+(const Vec &other) {
+  return Vec(this->x + other.x, this->y + other.y);
+}
+Vec Vec::operator-(const Vec &other) {
+  return Vec(this->x - other.x, this->y - other.y);
+}
 
 Vec Vec::operator*(const float k) { return Vec(k * this->x, k * this->y); }
 
 Vec Vec::operator/(const float k) { return Vec(this->x / k, this->y / k); }
 
 Vec Vec::operator<<(const float angle) {
-	float x1 = this->x * cosf(angle) - this->y * sinf(angle);
-	float y1 = this->x * sinf(angle) + this->y * cosf(angle);
-	return Vec(x1, y1);
+  float x1 = this->x * cosf(angle) - this->y * sinf(angle);
+  float y1 = this->x * sinf(angle) + this->y * cosf(angle);
+  return Vec(x1, y1);
 }
 
 Vec Vec::operator>>(const float angle) {
-	float x1 = this->x * cosf(angle) + this->y * sinf(angle);
-	float y1 = -this->x * sinf(angle) + this->y * cosf(angle);
-	return Vec(x1, y1);
+  float x1 = this->x * cosf(angle) + this->y * sinf(angle);
+  float y1 = -this->x * sinf(angle) + this->y * cosf(angle);
+  return Vec(x1, y1);
 }
 
 mycolor get_color(char c) {
-	mycolor result;
-	switch (c) {
-		case 'w':
-			result = {1.0f, 1.0f, 1.0f};
-			break;  // white
-		case 'b':
-			result = {0.0f, 0.0f, 0.0f};
-			break;  // black
-		case 'r':
-			result = {1.0f, 0.0f, 0.0f};
-			break;  // red
-		case 'g':
-			result = {0.0f, 1.0f, 0.0f};
-			break;  // green
-		case 'l':
-			result = {0.0f, 0.0f, 1.0f};
-			break;  // blue
-		case 'y':
-			result = {1.0f, 1.0f, 0.0f};
-			break;  // yellow
-		case 'c':
-			result = {0.0f, 1.0f, 1.0f};
-			break;  // cyan
-		case 'm':
-			result = {1.0f, 0.0f, 1.0f};
-			break;  // magenta
-		case 'o':
-			result = {1.0f, 0.647f, 0.0f};
-			break;  // orange
-		case 'p':
-			result = {0.502f, 0.0f, 0.502f};
-			break;  // purple
-		default:
-			result = {0.0f, 0.0f, 0.0f};
-			break;  // default as black
-	}
-	return result;
+  mycolor result;
+  switch (c) {
+  case 'w':
+    result = {1.0f, 1.0f, 1.0f};
+    break; // white
+  case 'b':
+    result = {0.0f, 0.0f, 0.0f};
+    break; // black
+  case 'r':
+    result = {1.0f, 0.0f, 0.0f};
+    break; // red
+  case 'g':
+    result = {0.0f, 1.0f, 0.0f};
+    break; // green
+  case 'l':
+    result = {0.0f, 0.0f, 1.0f};
+    break; // blue
+  case 'y':
+    result = {1.0f, 1.0f, 0.0f};
+    break; // yellow
+  case 'c':
+    result = {0.0f, 1.0f, 1.0f};
+    break; // cyan
+  case 'm':
+    result = {1.0f, 0.0f, 1.0f};
+    break; // magenta
+  case 'o':
+    result = {1.0f, 0.647f, 0.0f};
+    break; // orange
+  case 'p':
+    result = {0.502f, 0.0f, 0.502f};
+    break; // purple
+  default:
+    result = {0.0f, 0.0f, 0.0f};
+    break; // default as black
+  }
+  return result;
 }
 
 Figure::Figure(Vec anchor, float angle, float u_length, mycolor color) {
-	this->anchor = anchor;
-	this->angle = angle;
-	this->u_length = u_length;
-	this->color = color;
+  this->anchor = anchor;
+  this->angle = angle;
+  this->u_length = u_length;
+  this->color = color;
 }
 
 Vec Figure::getAnchor() { return this->anchor; }
@@ -88,146 +92,163 @@ void Figure::rotate(float angle) { this->angle = this->angle + angle; }
 void Figure::zoom(float k) { this->u_length = this->u_length * k; }
 
 Circle::Circle(Vec anchor, float angle, float u_length, mycolor color) {
-	setAnchor(anchor);
-	setangle(angle);
-	setlen(u_length);
-	setcolor(color);
+  setAnchor(anchor);
+  setangle(angle);
+  setlen(u_length);
+  setcolor(color);
 }
 
 void Circle::draw() {
 #ifndef HEADLESS
-	glBegin(GL_TRIANGLE_FAN);
-	glColor3f(this->getcolor().r, this->getcolor().g, this->getcolor().b);
-	glVertex2f(this->getAnchor().getX(), this->getAnchor().getY());
-	for (int i = 0; i <= 3000; i++) {
-		float angle = static_cast<float>(2 * M_PI * i / 3000);
-		glVertex2f(this->getAnchor().getX() + this->getlen() * cosf(angle),
-		           this->getAnchor().getY() + this->getlen() * sinf(angle));
-	}
-	glEnd();
-	glFlush();
+  glBegin(GL_TRIANGLE_FAN);
+  glColor3f(this->getcolor().r, this->getcolor().g, this->getcolor().b);
+  glVertex2f(this->getAnchor().getX(), this->getAnchor().getY());
+  for (int i = 0; i <= 3000; i++) {
+    float angle = static_cast<float>(2 * M_PI * i / 3000);
+    glVertex2f(this->getAnchor().getX() + this->getlen() * cosf(angle),
+               this->getAnchor().getY() + this->getlen() * sinf(angle));
+  }
+  glEnd();
+  glFlush();
 #endif
 }
 
 Semicircle::Semicircle(Vec anchor, float angle, float u_length, mycolor color) {
-	setAnchor(anchor);
-	setangle(angle);
-	setlen(u_length);
-	setcolor(color);
+  setAnchor(anchor);
+  setangle(angle);
+  setlen(u_length);
+  setcolor(color);
 }
 
 void Semicircle::draw() {
 #ifndef HEADLESS
-	glBegin(GL_TRIANGLE_FAN);
-	glColor3f(this->getcolor().r, this->getcolor().g, this->getcolor().b);
-	glVertex2f(this->getAnchor().getX(), this->getAnchor().getY());
-	for (int i = -1500; i <= 1500; i++) {
-		float angle = static_cast<float>(M_PI * i / 3000);
-		glVertex2f(this->getAnchor().getX() + this->getlen() * cosf(angle),
-		           this->getAnchor().getY() + this->getlen() * sinf(angle));
-	}
-	glEnd();
-	glFlush();
+  glBegin(GL_TRIANGLE_FAN);
+  glColor3f(this->getcolor().r, this->getcolor().g, this->getcolor().b);
+  glVertex2f(this->getAnchor().getX(), this->getAnchor().getY());
+  for (int i = -1500; i <= 1500; i++) {
+    float angle = static_cast<float>(M_PI * i / 3000);
+    glVertex2f(this->getAnchor().getX() + this->getlen() * cosf(angle),
+               this->getAnchor().getY() + this->getlen() * sinf(angle));
+  }
+  glEnd();
+  glFlush();
 #endif
 }
 
 Triangle::Triangle(Vec anchor, float angle, float u_length, mycolor color) {
-	setAnchor(anchor);
-	setangle(angle);
-	setlen(u_length);
-	setcolor(color);
+  setAnchor(anchor);
+  setangle(angle);
+  setlen(u_length);
+  setcolor(color);
 }
 
 void Triangle::draw() {
 #ifndef HEADLESS
-	glBegin(GL_TRIANGLES);
-	glColor3f(this->getcolor().r, this->getcolor().g, this->getcolor().b);
-	glVertex2f(this->getAnchor().getX() + cosf(this->getangle()) * this->getlen(),
-	           this->getAnchor().getY() + sinf(this->getangle()) * this->getlen());
-	glVertex2f(this->getAnchor().getX() +
-	               cosf(this->getangle() + static_cast<float>(2 * M_PI / 3)) * this->getlen(),
-	           this->getAnchor().getY() +
-	               sinf(this->getangle() + static_cast<float>(2 * M_PI / 3)) * this->getlen());
-	glVertex2f(this->getAnchor().getX() +
-	               cosf(this->getangle() - static_cast<float>(2 * M_PI / 3)) * this->getlen(),
-	           this->getAnchor().getY() +
-	               sinf(this->getangle() - static_cast<float>(2 * M_PI / 3)) * this->getlen());
-	glEnd();
-	glFlush();
+  glBegin(GL_TRIANGLES);
+  glColor3f(this->getcolor().r, this->getcolor().g, this->getcolor().b);
+  glVertex2f(this->getAnchor().getX() + cosf(this->getangle()) * this->getlen(),
+             this->getAnchor().getY() +
+                 sinf(this->getangle()) * this->getlen());
+  glVertex2f(this->getAnchor().getX() +
+                 cosf(this->getangle() + static_cast<float>(2 * M_PI / 3)) *
+                     this->getlen(),
+             this->getAnchor().getY() +
+                 sinf(this->getangle() + static_cast<float>(2 * M_PI / 3)) *
+                     this->getlen());
+  glVertex2f(this->getAnchor().getX() +
+                 cosf(this->getangle() - static_cast<float>(2 * M_PI / 3)) *
+                     this->getlen(),
+             this->getAnchor().getY() +
+                 sinf(this->getangle() - static_cast<float>(2 * M_PI / 3)) *
+                     this->getlen());
+  glEnd();
+  glFlush();
 #endif
 }
 
-Right_Triangle::Right_Triangle(Vec anchor, float angle, float u_length, mycolor color) {
-	setAnchor(anchor);
-	setangle(angle);
-	setlen(u_length);
-	setcolor(color);
+Right_Triangle::Right_Triangle(Vec anchor, float angle, float u_length,
+                               mycolor color) {
+  setAnchor(anchor);
+  setangle(angle);
+  setlen(u_length);
+  setcolor(color);
 }
 
 void Right_Triangle::draw() {
 #ifndef HEADLESS
-	glBegin(GL_TRIANGLES);
-	glColor3f(this->getcolor().r, this->getcolor().g, this->getcolor().b);
-	glVertex2f(this->getAnchor().getX() + cosf(this->getangle()) * this->getlen(),
-	           this->getAnchor().getY() + sinf(this->getangle()) * this->getlen());
-	glVertex2f(this->getAnchor().getX() +
-	               cosf(this->getangle() + static_cast<float>(2 * M_PI / 3)) * this->getlen(),
-	           this->getAnchor().getY() +
-	               sinf(this->getangle() + static_cast<float>(2 * M_PI / 3)) * this->getlen());
-	glVertex2f(
-	    this->getAnchor().getX() + (cosf(this->getangle() + static_cast<float>(2 * M_PI / 3)) +
-	                                cosf(this->getangle() - static_cast<float>(2 * M_PI / 3))) *
-	                                   this->getlen() / 2,
-	    this->getAnchor().getY() + (sinf(this->getangle() - static_cast<float>(2 * M_PI / 3)) +
-	                                sinf(this->getangle() + static_cast<float>(2 * M_PI / 3))) *
-	                                   this->getlen() / 2);
-	glEnd();
-	glFlush();
+  glBegin(GL_TRIANGLES);
+  glColor3f(this->getcolor().r, this->getcolor().g, this->getcolor().b);
+  glVertex2f(this->getAnchor().getX() + cosf(this->getangle()) * this->getlen(),
+             this->getAnchor().getY() +
+                 sinf(this->getangle()) * this->getlen());
+  glVertex2f(this->getAnchor().getX() +
+                 cosf(this->getangle() + static_cast<float>(2 * M_PI / 3)) *
+                     this->getlen(),
+             this->getAnchor().getY() +
+                 sinf(this->getangle() + static_cast<float>(2 * M_PI / 3)) *
+                     this->getlen());
+  glVertex2f(this->getAnchor().getX() +
+                 (cosf(this->getangle() + static_cast<float>(2 * M_PI / 3)) +
+                  cosf(this->getangle() - static_cast<float>(2 * M_PI / 3))) *
+                     this->getlen() / 2,
+             this->getAnchor().getY() +
+                 (sinf(this->getangle() - static_cast<float>(2 * M_PI / 3)) +
+                  sinf(this->getangle() + static_cast<float>(2 * M_PI / 3))) *
+                     this->getlen() / 2);
+  glEnd();
+  glFlush();
 #endif
 }
 
-Rectangle::Rectangle(Vec anchor, float angle, float u_length, mycolor color, float angle1) {
-	setAnchor(anchor);
-	setangle(angle);
-	setlen(u_length);
-	setcolor(color);
-	this->angle1 = angle1;
+Rectangle::Rectangle(Vec anchor, float angle, float u_length, mycolor color,
+                     float angle1) {
+  setAnchor(anchor);
+  setangle(angle);
+  setlen(u_length);
+  setcolor(color);
+  this->angle1 = angle1;
 }
 
 void Rectangle::setangle1(float a) { this->angle1 = a; }
 
 void Rectangle::draw() {
 #ifndef HEADLESS
-	glBegin(GL_POLYGON);
-	glColor3f(this->getcolor().r, this->getcolor().g, this->getcolor().b);
-	glVertex2f(this->getAnchor().getX() + cosf(this->getangle() + this->angle1) * this->getlen(),
-	           this->getAnchor().getY() + sinf(this->getangle() + this->angle1) * this->getlen());
-	glVertex2f(this->getAnchor().getX() + cosf(this->getangle() - this->angle1) * this->getlen(),
-	           this->getAnchor().getY() + sinf(this->getangle() - this->angle1) * this->getlen());
-	glVertex2f(
-	    this->getAnchor().getX() +
-	        cosf(static_cast<float>(M_PI) + this->getangle() + this->angle1) * this->getlen(),
-	    this->getAnchor().getY() +
-	        sinf(static_cast<float>(M_PI) + this->getangle() + this->angle1) * this->getlen());
-	glVertex2f(
-	    this->getAnchor().getX() +
-	        cosf(static_cast<float>(M_PI) + this->getangle() - this->angle1) * this->getlen(),
-	    this->getAnchor().getY() +
-	        sinf(static_cast<float>(M_PI) + this->getangle() - this->angle1) * this->getlen());
-	glEnd();
-	glFlush();
+  glBegin(GL_POLYGON);
+  glColor3f(this->getcolor().r, this->getcolor().g, this->getcolor().b);
+  glVertex2f(this->getAnchor().getX() +
+                 cosf(this->getangle() + this->angle1) * this->getlen(),
+             this->getAnchor().getY() +
+                 sinf(this->getangle() + this->angle1) * this->getlen());
+  glVertex2f(this->getAnchor().getX() +
+                 cosf(this->getangle() - this->angle1) * this->getlen(),
+             this->getAnchor().getY() +
+                 sinf(this->getangle() - this->angle1) * this->getlen());
+  glVertex2f(this->getAnchor().getX() + cosf(static_cast<float>(M_PI) +
+                                             this->getangle() + this->angle1) *
+                                            this->getlen(),
+             this->getAnchor().getY() + sinf(static_cast<float>(M_PI) +
+                                             this->getangle() + this->angle1) *
+                                            this->getlen());
+  glVertex2f(this->getAnchor().getX() + cosf(static_cast<float>(M_PI) +
+                                             this->getangle() - this->angle1) *
+                                            this->getlen(),
+             this->getAnchor().getY() + sinf(static_cast<float>(M_PI) +
+                                             this->getangle() - this->angle1) *
+                                            this->getlen());
+  glEnd();
+  glFlush();
 #endif
 }
 
-Trapezium::Trapezium(Vec anchor, float angle, float u_length, mycolor color, float ratio,
-                     float angle1, float angle2) {
-	setAnchor(anchor);
-	setangle(angle);
-	setlen(u_length);
-	setcolor(color);
-	this->ratio = ratio;
-	this->angle1 = angle1;
-	this->angle2 = angle2;
+Trapezium::Trapezium(Vec anchor, float angle, float u_length, mycolor color,
+                     float ratio, float angle1, float angle2) {
+  setAnchor(anchor);
+  setangle(angle);
+  setlen(u_length);
+  setcolor(color);
+  this->ratio = ratio;
+  this->angle1 = angle1;
+  this->angle2 = angle2;
 }
 
 void Trapezium::setratio(float k) { this->ratio = k; }
@@ -236,27 +257,29 @@ void Trapezium::setangle2(float a) { this->angle2 = a; }
 
 void Trapezium::draw() {
 #ifndef HEADLESS
-	glBegin(GL_POLYGON);
-	glColor3f(this->getcolor().r, this->getcolor().g, this->getcolor().b);
-	glVertex2f(this->getAnchor().getX() +
-	               cosf(this->getangle() + this->angle1) * this->getlen() * this->ratio,
-	           this->getAnchor().getY() +
-	               sinf(this->getangle() + this->angle1) * this->getlen() * this->ratio);
-	glVertex2f(this->getAnchor().getX() +
-	               cosf(this->getangle() - this->angle1) * this->getlen() * this->ratio,
-	           this->getAnchor().getY() +
-	               sinf(this->getangle() - this->angle1) * this->getlen() * this->ratio);
-	glVertex2f(
-	    this->getAnchor().getX() +
-	        cosf(static_cast<float>(M_PI) + this->getangle() + this->angle2) * this->getlen(),
-	    this->getAnchor().getY() +
-	        sinf(static_cast<float>(M_PI) + this->getangle() + this->angle2) * this->getlen());
-	glVertex2f(
-	    this->getAnchor().getX() +
-	        cosf(static_cast<float>(M_PI) + this->getangle() - this->angle2) * this->getlen(),
-	    this->getAnchor().getY() +
-	        sinf(static_cast<float>(M_PI) + this->getangle() - this->angle2) * this->getlen());
-	glEnd();
-	glFlush();
+  glBegin(GL_POLYGON);
+  glColor3f(this->getcolor().r, this->getcolor().g, this->getcolor().b);
+  glVertex2f(this->getAnchor().getX() + cosf(this->getangle() + this->angle1) *
+                                            this->getlen() * this->ratio,
+             this->getAnchor().getY() + sinf(this->getangle() + this->angle1) *
+                                            this->getlen() * this->ratio);
+  glVertex2f(this->getAnchor().getX() + cosf(this->getangle() - this->angle1) *
+                                            this->getlen() * this->ratio,
+             this->getAnchor().getY() + sinf(this->getangle() - this->angle1) *
+                                            this->getlen() * this->ratio);
+  glVertex2f(this->getAnchor().getX() + cosf(static_cast<float>(M_PI) +
+                                             this->getangle() + this->angle2) *
+                                            this->getlen(),
+             this->getAnchor().getY() + sinf(static_cast<float>(M_PI) +
+                                             this->getangle() + this->angle2) *
+                                            this->getlen());
+  glVertex2f(this->getAnchor().getX() + cosf(static_cast<float>(M_PI) +
+                                             this->getangle() - this->angle2) *
+                                            this->getlen(),
+             this->getAnchor().getY() + sinf(static_cast<float>(M_PI) +
+                                             this->getangle() - this->angle2) *
+                                            this->getlen());
+  glEnd();
+  glFlush();
 #endif
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,32 @@
-#include <iostream>
 #include "figure.h"
+#include <iostream>
 
 void TimeStep(int n) {
-	glutTimerFunc(static_cast<unsigned int>(n), TimeStep, n);
-	glutPostRedisplay();
+  glutTimerFunc(static_cast<unsigned int>(n), TimeStep, n);
+  glutPostRedisplay();
 }
 
 void gldraw() {
-    static Semicircle c1;
-    glClear(GL_COLOR_BUFFER_BIT);
-    c1.draw();
+  static Semicircle c1;
+  glClear(GL_COLOR_BUFFER_BIT);
+  c1.draw();
 
-	glutSwapBuffers();
-	glFlush();
+  glutSwapBuffers();
+  glFlush();
 }
 
-int main(int argc, char** argv) {
-	(void)argc;  // unused
-	(void)argv;
+int main(int argc, char **argv) {
+  (void)argc; // unused
+  (void)argv;
 #ifndef HEADLESS
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-	glutInitWindowSize(1000, 1000);
-	glutCreateWindow("Interstellar Parking Lot");
-	glClearColor(1.0, 1.0, 1.0, 0.0);
-	glutDisplayFunc(gldraw);
-	glutTimerFunc(25, TimeStep, 25);
-	glutMainLoop();
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+  glutInitWindowSize(1000, 1000);
+  glutCreateWindow("Interstellar Parking Lot");
+  glClearColor(1.0, 1.0, 1.0, 0.0);
+  glutDisplayFunc(gldraw);
+  glutTimerFunc(25, TimeStep, 25);
+  glutMainLoop();
 #endif
-	return 0;
+  return 0;
 }
